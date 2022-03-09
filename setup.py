415bdfa4882f9d3720ea8ad8e0844befa8c09ad3
opdo <pedrilidio@gmail.com>
2022-03-09 14:01:56 -0300

    0.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ PATH_HERE = Path(__file__).parent
 README = (PATH_HERE/"README.md").read_text()
 
 setup(name='pbct',
-      version='0.3.1',
+      version='0.3.2',
       description='Predictive Bi-Clustering Trees in Python.',
       long_description=README,
       long_description_content_type="text/markdown",
@@ -17,7 +17,7 @@ setup(name='pbct',
       scripts=['bin/PBCT'],
       zip_safe=False,
       install_requires=[
-          'pandas', 'numpy', 'numba', 'tqdm',
+          'pandas', 'numpy<=1.21', 'numba', 'tqdm',
       ],
       extras_require={
           'Tree visualization': ['graphviz', 'matplotlib'],
